469d56e0d3134f8a1f56d8ec9bd1758917de4aa7
Alan Nato <alanwnato@gmail.com>
2023-06-29 09:10:53 -0700

    I got yougit add .

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Script that makes a request to causes an specific response
-curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:You got me!"
+#curl -XPUT -d 'You got me!' -sL 0.0.0.0:5000/catch_me
+curl -sL 0.0.0.0:5000/catch_me_2 -X PUT -d "user_id=98" -H "Origin: School"
