c53f30830d261f4d08d8ab7634fd4bd840495c6d
Alan Nato <alanwnato@gmail.com>
2023-07-01 05:06:49 -0700

    You got me!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Script that makes a request to causes an specific response
-#curl -XPUT -d 'You got me!' -sL 0.0.0.0:5000/catch_me
 curl -sL 0.0.0.0:5000/catch_me_2 -X PUT -d "user_id=98" -H "Origin: School"
