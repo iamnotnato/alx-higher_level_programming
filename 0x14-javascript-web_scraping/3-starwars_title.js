e249f716843fb37bd950f57335ed1cdc97507e86
Alan Nato <alanwnato@gmail.com>
2023-07-25 15:29:37 +0000

    feat : Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+const id = process.argv[2];
+request.get(`https://swapi-api.hbtn.io/api/films/${id}`, (err, _, body) => {
+  body = body ? JSON.parse(body) : null;
+  console.log(body.title || err);
 });
