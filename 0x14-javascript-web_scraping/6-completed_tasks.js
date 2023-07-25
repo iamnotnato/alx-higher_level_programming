927320aaf998e62749ceb1394b20c1feb036a528
Alan Nato <alanwnato@gmail.com>
2023-07-25 23:16:52 +0000

    fix : How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,18 @@
 #!/usr/bin/node
-const process = require('process');
 const request = require('request');
 
-let url = process.argv[2];
-let data;
-let collected = {};
-
-request(url, function (error, response, body) {
-  if (error != null) {
-    console.log(error);
-  } else {
-    data = JSON.parse(body);
-    data.forEach(function (result) {
-      if (result['completed'] === true) {
-        let userid = result['userId'];
-        if (!(userid in collected)) {
-          collected[userid] = 0;
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    const resp = {};
+    const json = JSON.parse(body);
+    for (let i = 0; i < json.length; i++) {
+      if (json[i].completed === true) {
+        if (resp[json[i].userId] === undefined) {
+          resp[json[i].userId] = 0;
         }
-        collected[userid] += 1;
+        resp[json[i].userId]++;
       }
-    });
-    console.log(collected);
+    }
+    console.log(resp);
   }
 });
