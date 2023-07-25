1b37565c6051d88f79dac7dda11f77351bdf5a12
Alan Nato <alanwnato@gmail.com>
2023-07-25 15:33:11 +0000

    fix : Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
-request.get(url, (err, res, body) => {
-  if (err) console.log(err);
-  else {
-    const films = body ? JSON.parse(body) : null;
-    const isHere = films.results.filter(f => f.characters.filter(c => c.includes('/18/')).length);
-    console.log(isHere.length);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
