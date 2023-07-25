d0523f8839e97297fa8fffbd6543300d533522db
Alan Nato <alanwnato@gmail.com>
2023-07-25 23:14:51 +0000

    fix : How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,17 +1,25 @@
 #!/usr/bin/node
+const process = require('process');
 const request = require('request');
-const url = 'https://jsonplaceholder.typicode.com/todos';
-request (process.argv[2], function (error, response, body) {
- if (!error) {
-    const todos = JSON.parse(body);
-    let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
+
+let url = process.argv[2];
+let data;
+let collected = {};
+
+request(url, function (error, response, body) {
+  if (error != null) {
+    console.log(error);
+  } else {
+    data = JSON.parse(body);
+    data.forEach(function (result) {
+      if (result['completed'] === true) {
+        let userid = result['userId'];
+        if (!(userid in collected)) {
+          collected[userid] = 0;
+        }
+        collected[userid] += 1;
       }
     });
-    console.log(completed);
+    console.log(collected);
   }
 });
